f58192e57a65dac3d617c43adf8b3ad1ba01ae29
superMsp10 <pandeymahan34@gmail.com>
2017-04-22 13:28:58 -0400

    controls

---
+++ SmallWorld/Assets/WorldBuilder.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WorldBuilder : MonoBehaviour
+{
+    public GameObject planetIns;
+    public int planetNum;
+    public Transform planetsStart;
+
+    public float amplitude;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        for (int i = 0; i < planetNum; i++)
+        {
+            Instantiate(planetIns, Random.insideUnitSphere * (i + 1) * amplitude, Quaternion.identity, planetsStart);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y < -planetNum * amplitude)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
