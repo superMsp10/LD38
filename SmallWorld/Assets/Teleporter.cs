dff219c926ac8bd4dd973672fba2a16c20dd2064
superMsp10 <pandeymahan34@gmail.com>
2017-04-22 23:08:54 -0400

    Types of planets

---
+++ SmallWorld/Assets/Teleporter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Teleporter : MonoBehaviour
+{
+
+    public Transform teleTo;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        List<Teleporter> t = WorldBuilder.thisBuilder.allTeles;
+        Transform trans = t[Random.Range(0, t.Count)].transform;
+
+        c.transform.position = trans.position + Vector3.Normalize(c.GetComponent<Rigidbody>().velocity);
+    }
+
+
+}
--- SmallWorld/Assets/WorldBuilder.cs
+++ SmallWorld/Assets/WorldBuilder.cs
@@ -1,16 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class WorldBuilder : MonoBehaviour
 {
-    public GameObject planetIns;
     public int planetNum;
     public Transform planetsStart;
 
     public float amplitude;
     public GameObject player;
 
+    public List<Teleporter> allTeles = new List<Teleporter>();
+    public List<Text> allHints = new List<Text>();
+    public GameObject[] allPlanets;
+
+    public static WorldBuilder thisBuilder;
+
+    void Awake()
+    {
+        thisBuilder = this;
+    }
 
 
     // Use this for initialization
@@ -19,7 +30,14 @@ public class WorldBuilder : MonoBehaviour
 
         for (int i = 0; i < planetNum; i++)
         {
-            Instantiate(planetIns, Random.insideUnitSphere * (i + 1) * amplitude, Quaternion.identity, planetsStart);
+           GameObject g = (GameObject) Instantiate( allPlanets[Random.Range(0,allPlanets.Length)] , Random.insideUnitSphere * planetNum * amplitude, Quaternion.identity, planetsStart);
+            if(g.tag == "TelePlanet")
+            {
+                allTeles.Add(g.GetComponentInChildren<Teleporter>());
+            }else if(g.tag == "HintPlanet")
+            {
+                allHints.Add(g.GetComponentInChildren<Text>());
+            }
         }
     }
 
