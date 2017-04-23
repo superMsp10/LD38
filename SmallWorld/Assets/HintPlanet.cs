7d5af03de701c66ae733c4ca6a59d4c25a47756c
superMsp10 <pandeymahan34@gmail.com>
2017-04-23 14:16:46 -0400

    colour hints

---
+++ SmallWorld/Assets/HintPlanet.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class HintPlanet : MonoBehaviour
+{
+    int hintType;
+    public Text hintText;
+    Person theif;
+
+    // Use this for initialization
+    void Start()
+    {
+        hintType = Random.Range(0, 1);
+        hintText.text = "Hover over screen to reveal hint";
+   
+    }
+
+    void OnMouseEnter()
+    {
+        if (theif == null)
+        {
+            theif = WorldBuilder.thisBuilder.theif;
+        }
+        switch (hintType)
+        {
+            case 0:
+                hintText.text = "The theif's colour is: " + theif.colourName;
+                break;
+
+            case 1:
+                hintText.text = "The theif's planets is: " + theif.planetType;
+                break;
+
+            default:
+                hintText.text = "The theif's is on a planet: ";
+                break;
+        }
+    }
+}
---
+++ SmallWorld/Assets/Person.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Person : MonoBehaviour
+{
+    public bool theif = false;
+    public string personType;
+    public string planetType;
+    public string colourName = "";
+
+
+    public Color normal;
+    public Renderer mat;
+
+
+    void OnMouseOver()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (theif)
+            {
+              WorldBuilder.thisBuilder.EndGame();
+            }
+            else
+            {
+                StartCoroutine(WorldBuilder.thisBuilder.ShowDialog(5f, "Nope. Was not me."));
+            }
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        mat.material.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        mat.material.color = normal;
+    }
+
+}
